d49cb61c5c01f1be507d9ff4c7b3c9fd8db91b09
Sam Rosenshein <srosenshein1@gmail.com>
2016-10-31 16:32:30 -0400

    deploy

---
+++ webpack.prod.config.js
@@ -0,0 +1,24 @@
+var Webpack = require('webpack');
+var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'build');
+var mainPath = path.resolve(__dirname, 'app', 'index.js');
+
+var config = {
+
+	devtool: 'source-map',
+	entry: mainPath,
+	output: {
+		path: buildPath,
+		filename: 'index_bundle.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: [nodeModulesPath]
+		}]
+	}
+};
+
+module.exports = config;
